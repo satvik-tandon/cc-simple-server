c3ad6b5b877bb9c5969918549e12e233b4887418
Satvvik <satviktandon@gmail.com>
2025-01-30 20:39:28 -0500

    Initial Commit

--- cc_simple_server/server.py
+++ cc_simple_server/server.py
@@ -3,8 +3,10 @@ from fastapi import HTTPException
 from fastapi import status
 from cc_simple_server.models import TaskCreate
 from cc_simple_server.models import TaskRead
+from contextlib import contextmanager
 from cc_simple_server.database import init_db
 from cc_simple_server.database import get_db_connection
+
 
 # init
 init_db()
@@ -15,6 +17,15 @@ app = FastAPI()
 # Edit the code below this line
 ############################################
 
+@contextmanager
+def get_cursor():
+    db = get_db_connection()
+    cursor = db.cursor()
+    try:
+        yield cursor
+    finally:
+        db.commit()
+        db.close()
 
 @app.get("/")
 async def read_root():
@@ -27,59 +38,68 @@ async def read_root():
 # POST ROUTE data is sent in the body of the request
 @app.post("/tasks/", response_model=TaskRead)
 async def create_task(task_data: TaskCreate):
-    """
-    Create a new task
 
-    Args:
-        task_data (TaskCreate): The task data to be created
+    try:
+        with get_cursor() as cursor:
+            cursor.execute(
+                            "INSERT INTO tasks (title,description,completed) VALUES(?,?,?)",
+                            (task_data.title,task_data.description,task_data.completed)
+                            )
+            task_id = cursor.lastrowid
+            return TaskRead(id=task_id, title=task_data.title, description=task_data.description, completed=task_data.completed)
+    
+    except Exception as E:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error creating task: {str(E)}")
 
-    Returns:
-        TaskRead: The created task data
-    """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
 
 
 # GET ROUTE to get all tasks
 @app.get("/tasks/", response_model=list[TaskRead])
 async def get_tasks():
-    """
-    Get all tasks in the whole wide database
+  
+    try:
+        with get_cursor() as cursor:
+            cursor.execute("SELECT id, title, description, completed from tasks")
+            tasks=cursor.fetchall()
 
-    Args:
-        None
+            return [TaskRead(id=row[0], title=row[1], description=row[2], completed=row[3]) for row in tasks]
 
-    Returns:
-        list[TaskRead]: A list of all tasks in the database
-    """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+    except Exception as E:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error retrieving tasks: {str(E)}")
+        
 
 
 # UPDATE ROUTE data is sent in the body of the request and the task_id is in the URL
 @app.put("/tasks/{task_id}/", response_model=TaskRead)
 async def update_task(task_id: int, task_data: TaskCreate):
-    """
-    Update a task by its ID
+   
+    try:
+        with get_cursor() as cursor:
+            cursor.execute("UPDATE tasks SET title=?, description=?, completed=? WHERE id=?",
+                        (task_data.title,task_data.description,task_data.completed,task_id))
+            
+            if cursor.rowcount ==0:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Task with ID {task_id} not found.")
+            
+            return TaskRead(id=task_id, title=task_data.title, description=task_data.description,completed=task_data.completed)
 
-    Args:
-        task_id (int): The ID of the task to be updated
-        task_data (TaskCreate): The task data to be updated
+    except Exception as E:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="404 Not Found")
 
-    Returns:
-        TaskRead: The updated task data
-    """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
 
 
 # DELETE ROUTE task_id is in the URL
 @app.delete("/tasks/{task_id}/")
 async def delete_task(task_id: int):
-    """
-    Delete a task by its ID
+    
+    try:
+        with get_cursor() as cursor:
+            cursor.execute("DELETE from tasks where ID=?",(task_id,))
 
-    Args:
-        task_id (int): The ID of the task to be deleted
+            if cursor.rowcount ==0:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="404 Not Found")
+            
+            return {"message": f"Task {task_id} deleted successfully"}
 
-    Returns:
-        dict: A message indicating that the task was deleted successfully
-    """
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+    except Exception as E:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Error deleting task: {str(E)}")
